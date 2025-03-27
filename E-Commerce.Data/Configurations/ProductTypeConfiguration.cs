2d873ac5150b8b5ac6ca0b6ef728d41808e197d8
MoHannad1223 <mohanedemam408@gmail.com>
2025-03-27 23:53:42 +0200

    LayerDataToHandleDatabase

---
+++ E-Commerce.Data/Configurations/ProductTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Data.DataOrEntity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Data.Configurations
+{
+    public class ProductTypeConfiguration : IEntityTypeConfiguration<ProductType>
+    {
+        public void Configure(EntityTypeBuilder<ProductType> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
