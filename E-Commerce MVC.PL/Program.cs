4b78767836bb77b65064e60f8cf716f9afff52f8
Ahmed <ahmedssacount404@gmail.com>
2025-05-15 01:28:17 +0300

    update the views to be responsive with the product controller

--- E-Commerce MVC.PL/Program.cs
+++ E-Commerce MVC.PL/Program.cs
@@ -1,85 +1,86 @@
-//using E_Commerce.Data.Context;
-//using E_Commerce.Services.FormFiles;
-//using E_Commerce_.Repository.Interfaces;
-//using E_Commerce_.Repository.Repository;
-//using Microsoft.AspNetCore.Hosting;
-//using Microsoft.EntityFrameworkCore;
+using E_Commerce.Data.Context;
+using E_Commerce.Services.FormFiles;
+using E_Commerce_.Repository.Interfaces;
+using E_Commerce_.Repository.Repository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 
-//namespace E_Commerce_MVC.PL
-//{
-//    public class Program
-//    {
-
-
-
-//        public static void Main(string[] args)
-//        {
-//            var builder = WebApplication.CreateBuilder(args);
-//            builder.Services.AddControllersWithViews();
-//            builder.Services.AddSingleton<IFormFile, FormFile>();
-
-
-//            // Add services to the container.
-//            builder.Services.AddControllersWithViews();
-//            builder.Services.AddDbContext<E_CommerceDbContext>(options => {
-
-//                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-//            });
-//            builder.Services.AddScoped<IProductRepository, ProductRepository>();
-//            builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
-//            builder.Services.AddScoped<IFileService, FileService>();
-
-//            var app = builder.Build();
-
-//            // Configure the HTTP request pipeline.
-//            if (!app.Environment.IsDevelopment())
-//            {
-//                app.UseExceptionHandler("/Home/Error");
-//                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//                app.UseHsts();
-//            }
-
-//            app.UseHttpsRedirection();
-//            app.UseStaticFiles();
-
-//            app.UseRouting();
-
-//            app.UseAuthorization();
-
-//            app.MapControllerRoute(
-//                name: "default",
-//                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//            app.Run();
-//        }
-//    }
-//}
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace E_Commerce_MVC.PL
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-    app.UseHttpsRedirection();
+    public class Program
+    {
+
+
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            // builder.Services.AddSingleton<IFormFile, FormFile>();
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<E_CommerceDbContext>(options => {
+
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+            });
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IFileService, FileService>();
+            builder.Services.AddHttpClient();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
+//var builder = WebApplication.CreateBuilder(args);
 
-app.UseStaticFiles();
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
 
-app.UseRouting();
+//var app = builder.Build();
 
-app.UseAuthorization();
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//    app.UseHttpsRedirection();
+//}
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.UseStaticFiles();
 
-app.Run();
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.Run();
 
